2d00a5d151d098d84741239da8e492a96d622f0c
rudy feng <rudy.feng@shopee.com>
2022-01-11 15:50:06 +0800

    feat(button): add function

--- packages/button/src/index.js
+++ packages/button/src/index.js
@@ -3,3 +3,18 @@ import _ from 'loadsh'
 export const getTime = () => {
   return _.now()
 }
+
+export const getCookie = (name) => {
+  const nameEQ = name + '='
+  const ca = document.cookie.split(';')
+  for (const item of ca) {
+    let c = item
+    while (c.charAt(0) === ' ') {
+      c = c.substring(1, c.length)
+    }
+    if (c.indexOf(nameEQ) === 0) {
+      return c.substring(nameEQ.length, c.length)
+    }
+  }
+  return ''
+}
